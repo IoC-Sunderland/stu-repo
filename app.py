c8b5942bf3cfa54dd5b3f48b0f01b3239897067c
IoC-Sunderland <gavin.mcclary@sunderland.ac.uk>
2020-11-27 17:54:09 +0000

    For Stu

---
+++ app.py
@@ -0,0 +1,37 @@
+# Import Flask and render-template.
+from flask import Flask, render_template, request, redirect
+app=Flask(__name__)
+
+# Add function for Route 'Sign-Up'
+
+@app.route("/sign-up", methods=["GET", "POST"])
+def sign_up():
+    if request.method == "POST":
+
+        req = request.form
+        print(req)
+
+        return redirect('/')
+
+    return render_template('signup.html', title = 'Sign-Up')
+
+# Add function for Home
+
+@app.route("/data", methods=['GET', 'POST'])
+def data():
+    req = request.form
+    name = req.get("name")
+    dept = req.get("dept")
+    email = req.get("email")
+    password = req.get("password")
+
+    my_dict = {'name': name,
+               'dept': dept,
+               'email': email,
+               'password': password}
+
+    print(req)
+    return render_template('data.html', title = 'Dictionary Data', my_data=my_dict)
+
+
+app.run(debug=True)
